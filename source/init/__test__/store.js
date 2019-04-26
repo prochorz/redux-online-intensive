5eaea4b21c6a16d160d6ec9e19ce8d670bf4679c
prochor <prochorz@gmail.com>
2019-04-26 13:50:38 +0300

    LS 10

---
+++ source/init/__test__/store.js
@@ -0,0 +1,31 @@
+import {createStore, combineReducers} from 'redux';
+
+import {routerReducer as router} from 'react-router-redux';
+
+import {postsReducer as posts} from '../../bus/posts/reducer';
+import {uiReducer as ui} from '../../bus/ui/reducer';
+import {authReducer as auth} from '../../bus/auth/reducer';
+import {profileReducer as profile} from '../../bus/profile/reducer';
+import {usersReducer as users} from '../../bus/users/reducer';
+import {formsReducer as forms } from '../../bus/forms/reducer';
+
+//Store
+import {store} from '../store';
+
+const referenceRootReducer = combineReducers({
+  auth,
+  profile,
+  posts,
+  ui,
+  users,
+  router,
+  forms,
+})
+
+const referenceStore = createStore(referenceRootReducer);
+
+describe('store:', ()=>{
+  test('should have valid state shape', ()=>{
+    expect(store.getState()).toEqual(referenceStore.getState())
+  })
+});
